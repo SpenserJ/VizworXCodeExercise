2e2f7b4d3ec2245187589e73f0444e20ccf81cae
Spenser Jones <hello@spenserj.com>
2017-11-23 12:38:58 -0700

    Write a simple index file for customizing input

--- app/server/index.js
+++ app/server/index.js
@@ -1,2 +1,47 @@
-import Path from 'path';
-console.log('Test');
+import Restaurant from './Restaurant';
+import OrderBruteForce from './ordering/OrderBruteForce';
+
+/**
+ * Edit the input variable to modify a company's needs
+ */
+
+const input = {
+  requirements: {
+    total: 50,
+    specialization: {
+      veggie: 5,
+      gluten: 7,
+    },
+  },
+  restaurants: {
+    'Restaurant A': {
+      total: 40,
+      specialization: { veggie: 4 },
+      rating: 5,
+    },
+    'Restaurant B': {
+      total: 100,
+      specialization: { veggie: 20, gluten: 20 },
+      rating: 3,
+    },
+  },
+};
+
+/**
+ * Everything below here is for setting up class instances and calculating output
+ */
+
+const restaurants = Object.entries(input.restaurants).map(([name, details]) => (
+  new Restaurant(name, details.rating, details.total, details.specialization)
+));
+
+const order = new OrderBruteForce(
+  // Input is the total number of meals, but our order system calculates the number of other meals
+  // Subtract all of the special meals from the total before instantiating the class
+  input.requirements.total
+    - Object.values(input.requirements.specialization).reduce((acc, v) => (acc + v), 0),
+  input.requirements.specialization,
+);
+
+const result = order.calculateOrders(...restaurants);
+console.log(result);
