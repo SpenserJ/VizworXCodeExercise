eb93b51f93ef11bf5678be2a93cfa110a81b049e
Spenser Jones <hello@spenserj.com>
2017-11-23 08:21:23 -0700

    Switching from .mjs to .js
    .mjs is the new extension to indicate to Node that these are ES modules,
    instead of CJS modules, however support does not seem to exist for
    reliably loading them without specifying the extension if only .mjs
    exist. To reduce the potential of issues, I am switching all code to use
    .js instead, and relying on babel to transpile them to CJS modules.

---
+++ test/unit/app/server/restaurant.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+import { assert } from 'chai';
+
+import Restaurant from '../../../../app/server/restaurant';
+
+describe('Restaurant', () => {
+  it('should instantiate with an object properly', () => {
+    assert.fail(false, false, 'No tests are written yet');
+  });
+});
