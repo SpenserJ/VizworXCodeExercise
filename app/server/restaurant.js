ed90c38445f3d13c8a5515b7eb3561ec715871ff
Spenser Jones <hello@spenserj.com>
2017-11-23 08:46:10 -0700

    Test and logic for Restaurant class

--- app/server/restaurant.js
+++ app/server/restaurant.js
@@ -1,2 +1,26 @@
 export default class Restaurant {
+  constructor(name, meals, { veggie = 0, gluten = 0 } = {}) {
+    if (meals < 0 || veggie < 0 || gluten < 0) {
+      throw new Error('Cannot instantiate Restaurant with negative meals');
+    }
+    this._name = name;
+    this._meals = meals;
+    this._veggie = veggie;
+    this._gluten = gluten;
+  }
+
+  getRemainingMeals() { return this._meals; }
+  getRemainingVeggie() { return this._veggie; }
+  getRemainingGluten() { return this._gluten; }
+
+  orderMeal(specialized = false) {
+    if (this._meals <= 0) { return false; }
+    this._meals -= 1;
+    if (specialized === 'veggie') {
+      this._veggie -= 1;
+    } else if (specialized === 'gluten') {
+      this._gluten -= 1;
+    }
+    return true;
+  }
 }
--- test/unit/app/server/restaurant.js
+++ test/unit/app/server/restaurant.js
@@ -1,10 +1,42 @@
 /* eslint-env mocha */
-import { assert } from 'chai';
+import { expect } from 'chai';
 
 import Restaurant from '../../../../app/server/restaurant';
 
 describe('Restaurant', () => {
-  it('should instantiate with an object properly', () => {
-    assert.fail(false, false, 'No tests are written yet');
+  it('should instantiate with no specialized meals', () => {
+    const restaurant = new Restaurant('Test', 50);
+    expect(restaurant.getRemainingMeals()).to.equal(50);
+    expect(restaurant.getRemainingVeggie()).to.equal(0);
+    expect(restaurant.getRemainingGluten()).to.equal(0);
+  });
+
+  it('should instantiate with specialized meals', () => {
+    const restaurant = new Restaurant('Test', 50, { veggie: 5, gluten: 7 });
+    expect(restaurant.getRemainingMeals()).to.equal(50);
+    expect(restaurant.getRemainingVeggie()).to.equal(5);
+    expect(restaurant.getRemainingGluten()).to.equal(7);
+  });
+
+  it('should throw an exception if instantiated with negative meals', () => {
+    expect(() => new Restaurant('Test', -1)).to.throw();
+    expect(() => new Restaurant('Test', 50, { veggie: -1 })).to.throw();
+    expect(() => new Restaurant('Test', 50, { gluten: -1 })).to.throw();
+  });
+
+  it('should update when a veggie meal is ordered', () => {
+    const restaurant = new Restaurant('Test', 50, { veggie: 5, gluten: 7 });
+    restaurant.orderMeal('veggie');
+    expect(restaurant.getRemainingMeals()).to.equal(49);
+    expect(restaurant.getRemainingVeggie()).to.equal(4);
+    expect(restaurant.getRemainingGluten()).to.equal(7);
+  });
+
+  it('should update when a gluten meal is ordered', () => {
+    const restaurant = new Restaurant('Test', 50, { veggie: 5, gluten: 7 });
+    restaurant.orderMeal('gluten');
+    expect(restaurant.getRemainingMeals()).to.equal(49);
+    expect(restaurant.getRemainingVeggie()).to.equal(5);
+    expect(restaurant.getRemainingGluten()).to.equal(6);
   });
 });
