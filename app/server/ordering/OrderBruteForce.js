6a4898852988577a360225cde86c6b02e59250de
Spenser Jones <hello@spenserj.com>
2017-11-23 10:51:30 -0700

    Initial test and logic for bruteforce ordering

--- app/server/ordering/OrderBruteForce.js
+++ app/server/ordering/OrderBruteForce.js
@@ -1,7 +1,35 @@
 import OrderBase from './OrderBase';
+import { capFirstLetter } from '../MealBase';
 
+/**
+ * OrderBruteForce will attempt to order veggie meals first, then gluten-free
+ * meals, and finally non-specialized meals. If it is unable to complete the
+ * order, it will reset and order gluten-free before veggie.
+ */
 export default class OrderBruteForce extends OrderBase {
   calculateOrders(...restaurantsRaw) {
     const restaurants = super.calculateOrders(restaurantsRaw);
+
+    const loopRestaurants = (specializationOrder) => {
+      specializationOrder.forEach((specialization) => {
+        restaurants.forEach((restaurant) => {
+          if (this[`getRemaining${capFirstLetter(specialization)}`]() === 0) {
+            return;
+          }
+          this.orderMeal(restaurant, specialization);
+        });
+      });
+
+      if (this.getRemainingMeals() > 0) { return false; }
+      return true;
+    };
+
+    if (loopRestaurants(['veggie', 'gluten', 'other']) === false) {
+      if (loopRestaurants(['gluten', 'veggie', 'other']) === false) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
--- test/integration/OrderBruteForce.js
+++ test/integration/OrderBruteForce.js
@@ -14,4 +14,21 @@ describe('Brute Force', () => {
     const order = new OrderBruteForce(2, { veggie: 1, gluten: 1 });
     expect(() => order.calculateOrders(r1, r2)).to.not.throw();
   });
+
+  it('should return false in an impossible situation', () => {
+    const r1 = new Restaurant('Test 1', 0);
+    const r2 = new Restaurant('Test 2', 0);
+    const order = new OrderBruteForce(10);
+    expect(order.calculateOrders(r1, r2)).to.equal(false);
+  });
+
+  it('should return an object containing orders if successful', () => {
+    const r1 = new Restaurant('Test 1', 6);
+    const r2 = new Restaurant('Test 2', 6);
+    const order = new OrderBruteForce(10);
+    expect(order.calculateOrders(r1, r2)).to.equal({
+      'Test 1': { other: 6 },
+      'Test 2': { other: 4 },
+    });
+  })
 });
